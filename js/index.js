ef0ead0424f1030924b1d066315465e2dd0b60e8
coolarec <3187170085@qq.com>
2023-12-24 00:18:09 +0800

    增加二级评论功能与查看昵称功能

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+var pid;
 function searchInfo() {
     var continer = document.querySelector('#talk');
     var xhr = new XMLHttpRequest();
@@ -12,16 +13,38 @@ function searchInfo() {
                 continer.innerHTML = '';
                 continer.appendChild(p);
                 res.forEach(function (data) {
+                    var div = document.createElement('div');
+                    div.id = data.objectId;
+                    div.setAttribute('nick', data.nick);
+
                     var section = document.createElement('section');
                     section.className = 'message -left';
 
 
                     var divBalloon = document.createElement('div');
                     divBalloon.className = 'nes-balloon from-left';
-                    divBalloon.innerHTML = data.comment; // 使用评论数据中的内容
+                    divBalloon.innerHTML = data.comment;
 
+                    // 添加点击事件监听器
+                    divBalloon.addEventListener('click', function () {
+                        infodiv(div.id,'回复给→'+div.getAttribute('nick'));
+                    });
+
+                    var child = data['children'];
                     section.appendChild(divBalloon);
-                    continer.appendChild(section);
+                    div.appendChild(section);
+                    
+                    child.forEach(function (childdata) {
+                        var section1 = document.createElement('section');
+                        section1.className = 'message -right';
+                        var div1 = document.createElement('div');
+                        div1.className = 'nes-balloon from-right';
+                        div1.innerHTML = childdata.comment;
+                        section1.appendChild(div1);
+                        div.appendChild(section1);
+                    });
+
+                    continer.appendChild(div);
 
                 });
 
@@ -31,15 +54,15 @@ function searchInfo() {
         }
     };
 
-    xhr.open('GET', 'https://talkw.coolarec.link/api/comment?path=me&count=50', true);
+    xhr.open('GET', 'https://talkw.coolarec.link/api/comment?path=me&pageSize=50', true);
     xhr.send();
 }
-function send() {
+function sendInfo() {
     const nickname = document.getElementById('nickname').value || "匿名";
     const userComment = document.getElementById('comment').value;
 
     const xhr = new XMLHttpRequest();
-    const url = 'https://talkw.coolarec.link/comment?lang=zh-CN'; // 替换为实际的后端接收 POST 请求的 URL
+    const url = 'https://talkw.coolarec.link/comment'; // 替换为实际的后端接收 POST 请求的 URL
     xhr.open('POST', url, true);
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.onreadystatechange = function () {
@@ -56,9 +79,9 @@ function send() {
             }
         }
     };
-    const data = JSON.stringify({ "comment": userComment, "nick": nickname, "mail": "", "link": "", "url": "me", "ua": "" });
+    const data = JSON.stringify({ 'at':'匿名',"comment": userComment, "mail": "","nick": nickname,"pid": pid ,'rid':pid,'ua':"",'url':"me"});
     xhr.send(data);
-    layer.close(index);
+    layer.closeAll();
 }
 
 win10 = function (win10text) {
@@ -66,7 +89,31 @@ win10 = function (win10text) {
         skin: 'layui-layer-win10', // 2.8+
         shade: 0.01,
         btn: ['确定', '取消'],
-        title:"警告",
-        content:win10text,
+        title: "警告",
+        content: win10text,
     })
 };
+function infodiv(mypid,tname) {
+    pid=mypid;
+    layer.open({
+        type: 1,
+        offset: 't',
+        anim: 'slideDown', // 从上往下
+        area: ['100%', '100%'],
+        shade: 0.1,
+        shadeClose: true,
+        skin: 'sendinfo',
+        title: tname,
+        id: 'ID-demo-layer-direction-t',
+        content: '<div style="width: 80%;margin: 20px auto;" class="nes-container is-dark with-title" id="myElement">\
+                <p class="title">发送消息🚀</p>\
+                <div class="nes-field">\
+                <label for="name_field">不填默认匿名</label>\
+                <input type="text" id="nickname" class="nes-input">\
+            </div>\
+            <label for="textarea_field">写点什么吧<i class="snes-jp-logo"></i></label>\
+            <textarea id="comment" class="nes-textarea" rows="10"  placeholder="评论开启了审核系统，所以并不会实时显示" style="font-size:17px;"></textarea>\
+            <button type="button" class="nes-btn is-primary" onclick="sendInfo()">发送🙂</button>\
+        </div>'
+    });
+}
